3a9ce8dbd01a559ed455bcf138007575476970a3
Jason Martocci <Jason Martocci>
2016-02-16 16:47:02 -0500

    fixed prompt that says if you guessed the word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,30 +4,33 @@ var words = ['jason', 'kaitlyn', 'lora', 'matthew'];
 
 
 //Computer picks random word to guess
-function getItem(){
+function getItem() {
 	document.getElementById("wordGuess").innerHTML = words[Math.floor(Math.random() * words.length)];
 }
 
 //This code captures the keypress and prints it out on the screen
 var guessesLeft = 9;
 
-document.onkeypress = function(evt) {
-    var evt = evt || window.event;
-    var charCode = evt.keyCode || evt.which;
-    var lettersGuessed = String.fromCharCode(charCode);
-	document.getElementById("lettersGuessed").innerHTML += lettersGuessed;
-	document.getElementById("guessesLeft").innerHTML = guessesLeft;
-	guessesLeft --;
-
+document.onkeypress = function(keyPressed) {
+    var keyPressed = keyPressed || window.event,
+	    charCode = keyPressed.keyCode || keyPressed.which,
+	    lettersGuessed = String.fromCharCode(charCode);
+	
 	// var userGuess = prompt("What word do you guess?");
-	// if (words.indexOf(userGuess) > -1){
+	// var userGuess = words.split('');
+	// var userGuess
+	// if (words.indexOf(userGuess) > -1) {
 	// 	alert("Your guess is correct.")
-	// }else{
+	// }else {
 	// 	alert("Your guess is wrong.")
 	// }
 
+	document.getElementById("lettersGuessed").innerHTML += lettersGuessed;
+	document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
-	if (guessesLeft === -1){
+	guessesLeft--;
+
+	if (guessesLeft === -1) {
 		alert("You Loose!");
 	}
 };
@@ -49,7 +52,7 @@ function removeLetter(i) {
 	document.getElementById("guessesLeft").innerHTML = + guessesLeft;
 	guessesLeft --;
 
-	if (guessesLeft === -1){
+	if (guessesLeft === -1) {
 		alert("You Loose!");
 	}
 }
@@ -57,8 +60,8 @@ function removeLetter(i) {
 //Replaces the letter with blank after it is clicked or selected
 function replaceLetter(x) {
 	for (i=0; i<words.length; i++) {
-		var ul = document.getElementById("wordGuess");
-		var liArray = ul.getElementsByTagName("li");
+		var ul = document.getElementById("wordGuess"),
+			liArray = ul.getElementsByTagName("li");
 		if (words[i] == x ) {
 			liArray[i].innerHTML = x;
 		 }
