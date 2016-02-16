edf529387b9d536bc18cfd2be5d33c9ed7c0a1d2
Jason Martocci <Jason Martocci>
2016-02-16 16:09:35 -0500

    fixed letters guessed to display all in a row

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,31 @@
 //Possible words to guess
 var words = ['jason', 'kaitlyn', 'lora', 'matthew'];
-// var wordsLetters = function words.split(''){
 
-// }
+
 
 //Computer picks random word to guess
 function getItem(){
-    document.getElementById("wordGuess").innerHTML = words[Math.floor(Math.random() * words.length)];
+	document.getElementById("wordGuess").innerHTML = words[Math.floor(Math.random() * words.length)];
 }
 
 //This code captures the keypress and prints it out on the screen
 var guessesLeft = 9;
 
 document.onkeypress = function(evt) {
-    evt = evt || window.event;
+    var evt = evt || window.event;
     var charCode = evt.keyCode || evt.which;
     var lettersGuessed = String.fromCharCode(charCode);
-    document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
-	document.getElementById("guessesLeft").innerHTML = + guessesLeft;
+	document.getElementById("lettersGuessed").innerHTML += lettersGuessed;
+	document.getElementById("guessesLeft").innerHTML = guessesLeft;
 	guessesLeft --;
+
+	// var userGuess = prompt("What word do you guess?");
+	// if (words.indexOf(userGuess) > -1){
+	// 	alert("Your guess is correct.")
+	// }else{
+	// 	alert("Your guess is wrong.")
+	// }
+
 
 	if (guessesLeft === -1){
 		alert("You Loose!");
